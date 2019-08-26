22702175e86e0c24f46bb2c2b8875858698c772a
petshipproyecto <52719956+petshipproyecto@users.noreply.github.com>
2019-08-25 21:59:00 -0300

    Update perfil.js

--- src/models/perfil.js
+++ src/models/perfil.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataType) => {
     Perfil.associate = (models) => {
         Perfil.belongsTo(models.Raza, { foreignKey: 'Id_raza' });
         Perfil.belongsTo(models.Genero, { foreignKey: 'Id_genero'});
-        Perfil.belongsTo(models.Usuario, { foreignKey: 'Id_usuario'});
+        Perfil.belongsTo(models.Usuario, { foreignKey: 'Usr_cod'});
     };
   
     return Perfil;
