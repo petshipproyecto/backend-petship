75079a2d38594a63e6d5333894105f7bde84b288
nico <nicodiode@gmail.com>
2019-10-12 18:57:10 -0300

    Correcciones candidatos
    Obtiene todos los perfiles del mismo tipo de animal, de distinto usuario, y que no se haya descartado o aceptado con anterioridad.

--- src/routes/candidatos.js
+++ src/routes/candidatos.js
@@ -6,7 +6,17 @@ module.exports = app => {
     const Op = Sequelize.Op;
     const Match = app.db.models.Match;
     const Perfil = app.db.models.Perfil;
+    const Raza = app.db.models.Raza;
     
+    function perfil (Id_perfil) {
+        return new Promise(function (resolve, reject){
+            Perfil.findOne({
+                where: {Id_perfil: Id_perfil},
+                include: [{ model: Raza}]
+            }).then(result => {resolve(result)})
+        })
+    }
+
     function descartados (Id_perfil) {
         return new Promise(function (resolve, reject){
             Match.findAll({
@@ -17,30 +27,37 @@ module.exports = app => {
                 .then(result => {resolve(result.map(result => result.Id_perfil_destino))})
                 /* .catch(error => {
                     res.status(412).json({msg: error.message});
-                }) */;
+                }) */
         })
     }
 
-    function candidatos (descartados) {
+    function candidatos (descartados, perfil) {
         return new Promise(function (resolve, reject){
             Perfil.findAll({
-                    where: {Id_perfil: {[Op.notIn]: descartados}}
+                    where: {
+                            Id_perfil: {[Op.notIn]: descartados},
+                            Id_usuario: {[Op.ne]: perfil.Id_usuario}
+                        },
+                    include: [{ model: Raza, where: {Id_animal: perfil.Raza.Id_animal}}]
                 })
-                .then(result => {resolve(result)})
+                .then(result => {
+                    resolve(result)
+                })
                 /* .catch(error => {
                     res.status(412).json({msg: error.message});
-                }) */;
+                }) */
         })
     }
 
     app.route('/candidatos')
         .get((req, res) => {
-            descartados(req.body.Id_perfil)
-            .then(descartados => {
-                var ids = Array();
-                candidatos (descartados)
+            perfil (req.body.Id_perfil)
+            .then(perfil => {
+                descartados (perfil.Id_perfil)
+                .then(descartados => {
+                    candidatos (descartados, perfil)
                     .then(candidatos => {res.json(candidatos)})
+                })
             })
         })
-      
   };
